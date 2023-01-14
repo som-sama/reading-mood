a4876010919b9a3d8938273613c6b27c29820552
soumdatta81 <soumdatta81@gmail.com>
2023-01-14 22:22:41 +0530

    structure added

---
+++ script.js
@@ -0,0 +1,8 @@
+function search() {
+    var input = document.getElementById("search-input").value;
+    // Send the input to the server to retrieve song recommendations from GPT-3
+    // ...
+    // Update the page with the song recommendations
+    // ...
+  }
+  
